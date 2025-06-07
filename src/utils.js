09e39640d6e3814b27a085a7a374f0199926d027
carsugar <carling.sugarman@tinuiti.com>
2025-06-06 22:53:45 -0700

    Missing utils

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 export class JsonResponse extends Response {
   constructor(body, init) {
     const jsonBody = JSON.stringify(body);
@@ -9,3 +11,23 @@ export class JsonResponse extends Response {
     super(jsonBody, init);
   }
 }
+
+export const DISCORD_API_BASE_URL = "https://discord.com/api/v10";
+
+export const parseSlashCommandOptions = (options) => {
+  const parsedOptions = {};
+  options.forEach((option) => (parsedOptions[option.name] = option.value));
+  return parsedOptions;
+};
+
+export const getGuildRoles = async (guild, env) => {
+  const rolesRes = await axios.get(
+    `${DISCORD_API_BASE_URL}/guilds/${guild}/roles`,
+    {
+      headers: {
+        Authorization: `Bot ${env.DISCORD_TOKEN}`,
+      },
+    }
+  );
+  return rolesRes.data;
+};
